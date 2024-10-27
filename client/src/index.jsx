8d814fb0cdb8027e9c7ddb3124969338d596b690
Yunseo Park <dnlprk7777@gmail.com>
2024-10-27 10:16:18 -0400

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route, BrowserRouter } from "react-router-dom";
 import React from 'react';
 import App from './App.jsx';
+import './reset.css';
 import './style.css';
 
 const root = createRoot(document.getElementById('root'))
